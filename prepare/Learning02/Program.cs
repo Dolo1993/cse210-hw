e081a652bd80291a1aa33108af315232a4fbd97a
Emmanuel DK Dolo <emmanueldkdolo1993@gmail.com>
2023-06-15 05:22:22 +0000

    completed learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Account Manager";
+        job1._company = "Away Ride and Delivery";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "  Teacher";
+        job2._company = "Casbe International School ";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emmanuel DK Dolo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }
+}
+
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}"); 
+        Console.WriteLine();
+    }
+}
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs;
+
+    public Resume()
+    {
+        _jobs = new List<Job>();
+    }
+
+    public void Display() 
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Resume of {_name}:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
     }
 }
