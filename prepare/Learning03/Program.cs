47845b3ba4d87eed55efb6cad96bef6cfa0f7e0e
Emmanuel DK Dolo <emmanueldkdolo1993@gmail.com>
2023-06-21 03:18:28 +0000

    completed prepare learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,81 @@
 using System;
 
+class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom;
+    }
+
+    public int GetNumerator()
+    {
+        return numerator;
+    }
+
+    public void SetNumerator(int top)
+    {
+        numerator = top;
+    }
+
+    public int GetDenominator()
+    {
+        return denominator;
+    }
+
+    public void SetDenominator(int bottom)
+    {
+        denominator = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return numerator + "/" + denominator;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue()); 
+        
+        Fraction f5 = new Fraction(2,4); 
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
     }
 }
