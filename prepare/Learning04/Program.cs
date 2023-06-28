4bd0fe9f2b53b2cc4842423e5debdee35b976a0c
Emmanuel DK Dolo <emmanueldkdolo1993@gmail.com>
2023-06-28 01:06:06 +0000

    finished learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,75 @@
-using System;
-
-class Program
+// Assignment class
+public
+class Assignment
 {
-    static void Main(string[] args)
+    protected string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
+
+// MathAssignment class
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
+
+// WritingAssignment class
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_studentName}";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Testing the classes 
+        
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19"); 
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine();
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation()); 
+         Console.WriteLine(); 
+        Console.WriteLine();
     }
 }
